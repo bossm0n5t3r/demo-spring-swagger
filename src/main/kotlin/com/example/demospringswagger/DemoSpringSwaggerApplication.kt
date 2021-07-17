f033263f09b94478dfe024cd3696f2013375d559
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-07-17 16:21:09 +0900

    :tada:

---
+++ src/main/kotlin/com/example/demospringswagger/DemoSpringSwaggerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demospringswagger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSpringSwaggerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoSpringSwaggerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demospringswagger/DemoSpringSwaggerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demospringswagger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringSwaggerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
