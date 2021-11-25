e6518f66316dab19d3abff53d4ed5256bf7cd4e2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-11-25 23:27:49 +0900

    feat: Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,16 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.springframework.boot:spring-boot-starter-webflux:2.5.6")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.2-native-mt")
+	developmentOnly("org.springframework.boot:spring-boot-devtools:2.5.6")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client:2.7.3")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6")
+	testImplementation("io.projectreactor:reactor-test:3.4.12")
 }
 
 tasks.withType<KotlinCompile> {
